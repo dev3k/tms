588295f70926ff212e903e7a843fe800e4934ebc
khalil <khalil.majdalawi@gmail.com>
2025-04-22 12:32:29 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-svh">
+                <Button>Click me</Button>
+            </div>
+        </>
+    )
+}
+
+export default App
